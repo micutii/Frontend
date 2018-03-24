35dbaec993c3fd7c864c762123e1fb21ab020d4d
unknown <micutii333@gmail.com>
2018-03-24 18:59:01 +0200

    pins and info window

--- Robo/Module/index.js
+++ Robo/Module/index.js
@@ -4,78 +4,67 @@ var myApp = angular.module("myApp", ["isteven-multi-select"]);
 
 myApp.controller('mapsController', function ($scope, $http) {
     $scope.selectedTypes = [];
-    $scope.types = [
-    {name: "Opera", maker: "(Opera Software)", ticked: true }
-    ];;
+    $scope.types = [];
+    var map = {};
+    var infowindows = [];
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser00.");
+    function displayMap(pins) {
+        if (navigator.geolocation && false) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            let position = {};
+            position.coords = {};
+            position.coords.latitude = 44.4377401;
+            position.coords.longitude = 25.9545541;
+            showPosition(position);
+        }
+    }
+    
+    function addPins() {
+       
+        //$scope.pins.forEach(function (pin) 
+        for (i = 0; i < $scope.pins.length; i++) {
+            let pin = $scope.pins[i];
+            let myLatLng = { lat: pin.latitude, lng: pin.longitude };
+            pinContent = '<div class="popup"><h3 class="name">' +
+                pin.name + '</h3><p class="title">' + pin.type +
+                '</p><div class="address">' + pin.address +
+                '</div><div class="contact">' + pin.contact +
+                '</div><p class="title description">' + pin.description +
+                '</p></div>';
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: pin.name,
+                icon: getMarkerImage("blue"),
+            });
+            marker.content = pinContent;
+            marker.index = i;
+            infowindows.push(new google.maps.InfoWindow({ content: pinContent }));
+            
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    closeInfowindows();
+                    infowindows[i].open(map, marker);
+                }
+            })(marker, i));
+        };
     }
 
-    var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-            '<div id="bodyContent">' +
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the ' +
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-            'Aboriginal people of the area. It has many springs, waterholes, ' +
-            'rock caves and ancient paintings. Uluru is listed as a World ' +
-            'Heritage Site.</p>' +
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-            '(last visited June 22, 2009).</p>' +
-            '</div>' +
-        '</div>';
-    var name = "Facultatea de Automatica si calculatoare";
-    var type = "College";
-    var address = "Splaiul Independentei 290";
-    var contact = "tel: 0755279129 fax: 0322211112";
-    var description = "infiinţată in 1967, Facultatea de Automatică \
-        şi Calculatoare are ca misiune realizarea unui \
-        mediu fertil pentru educaţie, cercetare şi inovare, \
-        factori cheie în dezvoltarea economiei bazate pe \
-        cunoaştere.In particular, misiunea Facultăţii de Automatică \
-        şi Calculatoare este aceea de a potenţa cercetarea ştiinţifică \
-        de nivel înalt, de a împărtăşi cunoaştere prin educaţie în \
-        domeniul Calculatoare şi Tehnologia Informaţiei şi în \
-        domeniul Ingineria Sistemelor, şi de a oferi studenţilor \
-        şi cadrelor didactice din facultate un mediu profesional \
-        şi social stimulativ, de elită.";
-    contentString = '<div class="popup"><h3 class="name">' +
-        name + '</h3><p class="title">' + type +
-        '</p><div class="address">' + address +
-        '</div><div class="contact">' + contact +
-        '</div><p class="title description">' + description +
-        '</p></div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+    function closeInfowindows (){
+        for(let i = 0; i < $scope.pins.length; i++)
+        infowindows[i].close();
+    }
     
     function showPosition(position) {
         var myLatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
 
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
             center: myLatLng,
             zoom: 10
         });
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!',
-            icon: getMarkerImage("blue"),
-        });
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
+
+        addPins();
     }
 
     var icons = new Array();
@@ -108,11 +97,14 @@ myApp.controller('mapsController', function ($scope, $http) {
         return icons[iconColor];
     }
 
-    $http.get('http://10.0.0.76:8080/api/pins/get').then(function (data, statusCode) {
-        console.log("statusCode: ", statusCode);
-        console.log("data: ", data);
-    }
-    , function (data, statusCode) {
+    $http.get('http://10.0.0.76:8080/api/pins/get').then(function (response) {
+        $scope.pins = response.data;
+        displayMap();
+    });
 
+    $http.get('http://10.0.0.76:8080/api/types/get').then(function (response) {
+        response.data.forEach(function (type) {
+            $scope.types.push({ name: type.typeName, id: type.idType, ticked: false });
+        })
     });
 });
