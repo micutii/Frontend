74436a0264858a5770bbdd3c04d387d8b4a9e0eb
micutii333 <micutii333@gmail.com>
2018-03-24 22:41:25 +0200

    login

--- Robo/Module/login.js
+++ Robo/Module/login.js
@@ -27,18 +27,58 @@ loginApp.controller('loginController', function ($scope, $http) {
     $scope.submitLogin = function () {
         if ($scope.loginForm.email == '' || $scope.loginForm.password == '') return;
         console.log($scope.loginForm);
+
+        function make_base_auth(user, password) {
+            var tok = user + ':' + password;
+            var hash = btoa(tok);
+            return "Basic " + hash;
+        }
+
+        var req = {
+            method: 'GET',
+            url: 'http://10.0.0.76:8080/api/login',
+            withCredentials: true,
+            headers: {
+                'Authorization': make_base_auth($scope.loginForm.email, $scope.loginForm.password)
+            }
+        }
+
+        $http(req).then(function (data) {
+            //$scope.username = $scope.log
+        }
+            , function (data) {
+
+            });
+
     }
 
     $scope.submitRegister = function () {
         if ($scope.registerForm.email == '' || $scope.registerForm.password == '' || $scope.registerForm.fullName == '') return;
         console.log($scope.registerForm);
-    }
+       
+        var req = {
+            method: 'GET',
+            url: 'http://10.0.0.76:8080/api/events/get',
+            withCredentials: true
+        }
 
+        $http(req).then(function (data, statusCode) {
+            console.log("statusCode: ", statusCode);
+            console.log("data: ", data);
+        }
+            , function (data, statusCode) {
+
+            });
+        
+    }
+    /*
     $http.get('http://10.0.0.76:8080/api/pins/get').then(function (data, statusCode) {
         console.log("statusCode: ", statusCode);
         console.log("data: ", data);
     }
+    
         , function (data, statusCode) {
 
         });
+    */
 });
