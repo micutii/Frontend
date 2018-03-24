6449b13db4ca4d934e06b728b4a460424937c1da
micutii333 <micutii333@gmail.com>
2018-03-24 18:52:11 +0200

    Added login functionality. Waiting for the backend

---
+++ Robo/Module/login.js
@@ -0,0 +1,44 @@
+﻿
+var loginApp = angular.module("loginApp", []);
+loginApp.controller('loginController', function ($scope, $http) {
+    $scope.selectedTypes = [];
+    $scope.showLogin = true;
+    $scope.showRegister = false;
+
+    $scope.toggleLogin = function () {
+        $scope.showLogin = !$scope.showLogin;
+    };
+
+    $scope.toggleRegister = function () {
+        $scope.showRegister = !$scope.showRegister;
+    };
+
+    $scope.loginForm = {
+        email: '',
+        password: ''
+    };
+
+    $scope.registerForm = {
+        fullName: '',
+        email: '',
+        password: ''
+    };
+
+    $scope.submitLogin = function () {
+        if ($scope.loginForm.email == '' || $scope.loginForm.password == '') return;
+        console.log($scope.loginForm);
+    }
+
+    $scope.submitRegister = function () {
+        if ($scope.registerForm.email == '' || $scope.registerForm.password == '' || $scope.registerForm.fullName == '') return;
+        console.log($scope.registerForm);
+    }
+
+    $http.get('http://10.0.0.76:8080/api/pins/get').then(function (data, statusCode) {
+        console.log("statusCode: ", statusCode);
+        console.log("data: ", data);
+    }
+        , function (data, statusCode) {
+
+        });
+});
