5d46728b849bc43f3f677090d1ed8cfbf1e169d7
unknown <micutii333@gmail.com>
2018-03-25 05:05:03 +0300

    fix

--- Robo/Module/index.js
+++ Robo/Module/index.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-=======
-﻿myApp = angular.module('myApp', ['ui.router']);
->>>>>>> 25e9701a47bc9bad24598d363946ca798b2e4153
-
-﻿myApp = angular.module('myApp', ['ui.router', "isteven-multi-select"]);
+myApp = angular.module('myApp', ['ui.router', "isteven-multi-select"]);
 myApp.config(function ($stateProvider) {
     var maps = {
         name: 'maps',
